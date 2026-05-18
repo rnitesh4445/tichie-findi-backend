62245e648fd58aa023e1b49c83b94f1e3b092f43
NITESH KUMAR <127005443+rnitesh4445@users.noreply.github.com>
2026-05-18 21:48:29 +0530

    Use environment variable for server portenvsetup

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
+require("dotenv").config();
+
 const express=require("express");
 const app=express();
 const connectDB=require("./config/database");
 const User=require("./models/user")
 const bcrypt=require("bcrypt")
 const cookieParser = require('cookie-parser')
-const  jwt=require("jsonwebtoken")
+const jwt=require("jsonwebtoken")
 const userAuth=require("./middleware/auth")
 const cors=require("cors")
+
 app.use(express.json());
 app.use(cookieParser())
+
 app.use(cors({
-    
     origin: "http://localhost:5173",
     credentials: true
 }));
+
 const profileUrlRouter = require("./router/profileUrl");
 const authRouter=require("./router/auth")
 const profileRouter=require("./router/profile")
@@ -26,8 +30,6 @@ app.use("/",requestRouter)
 app.use("/",userRouter)
 app.use("/", profileUrlRouter);
 
-
-
 connectDB().then(()=>{
 
 const PORT = process.env.PORT || 3000;
@@ -35,6 +37,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 });
+
 })
 .catch((err)=>{
     console.log("Error connecting to database: "+err.message)
