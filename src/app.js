6b734a9c617e174e6cfba270f472dbda9f794552
NITESH KUMAR <127005443+rnitesh4445@users.noreply.github.com>
2026-05-18 21:22:46 +0530

    Use environment variable for server portenvsetup

--- src/app.js
+++ src/app.js
@@ -30,10 +30,12 @@ app.use("/", profileUrlRouter);
 
 connectDB().then(()=>{
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
-})})
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
+})
 .catch((err)=>{
     console.log("Error connecting to database: "+err.message)
 })
