5b356b9e8bc9247f87b52ea1fb0c07470ea637fe
NITESH KUMAR <127005443+rnitesh4445@users.noreply.github.com>
2026-05-18 21:54:18 +0530

    Use environment variable for server portenvsetup

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,26 +1,36 @@
 const jwt=require("jsonwebtoken");
-const User=require("../models/user")
-const jwtSecret_key=require("../config/env").jwtSecret_key;
+const User=require("../models/user");
+
+const jwtSecret_key = process.env.jwtSecret_key;
 
 const userAuth=async(req,res,next)=>{
     try{
         const token=req.cookies.token;
+
         if(!token)
         {
             throw new Error("token is not present")
         }
-        const decode=jwt.verify(token,jwtSecret_key)
+
+        const decode=jwt.verify(token,jwtSecret_key);
+
         const {_id}=decode;
+
         const user=await User.findById(_id);
+
         if(!user)
         {
             throw new Error("user is not present")
         }
+
         req.user=user;
+
         next();
+
     }
     catch(err){
-        res.status(500).send("something went wrong"+err.message)
+        res.status(500).send("something went wrong "+err.message)
     }
 }
+
 module.exports=userAuth;
